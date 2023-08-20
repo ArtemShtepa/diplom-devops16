7dd067999b8b5d5bf7104253e4cd617539d23dea
ArtemShtepa <artemshtepa@ya.ru>
2023-08-20 23:44:17 +0300

    Настройка роутера для подсетей

--- tf/network.tf
+++ tf/network.tf
@@ -3,14 +3,6 @@ resource "yandex_vpc_network" "net-master" {
   name = "network-master"
 }
 
-# Подсеть основных ресурсов: bastion, vm-main-1, vm-main-2, vm-main-3
-resource "yandex_vpc_subnet" "subnet-main" {
-  name           = "subnet-main"
-  v4_cidr_blocks = ["192.168.1.0/24"]
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.net-master.id
-}
-
 # Виртуальный роутер
 resource "yandex_vpc_route_table" "net-router" {
   name       = "route-table"
@@ -22,12 +14,22 @@ resource "yandex_vpc_route_table" "net-router" {
   }
 }
 
+# Подсеть основных ресурсов: bastion, vm-main-1, vm-main-2, vm-main-3
+resource "yandex_vpc_subnet" "subnet-main" {
+  name           = "subnet-main"
+  v4_cidr_blocks = ["192.168.1.0/24"]
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.net-master.id
+  route_table_id = yandex_vpc_route_table.net-router.id
+}
+
 # Подсеть Kubernetes кластера в зоне доступности А
 resource "yandex_vpc_subnet" "subnet-kube-a" {
   name           = "subnet-kube-a"
   v4_cidr_blocks = ["192.168.10.0/24"]
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.net-master.id
+  route_table_id = yandex_vpc_route_table.net-router.id
 }
 
 # Подсеть Kubernetes кластера в зоне доступности Б
@@ -36,6 +38,7 @@ resource "yandex_vpc_subnet" "subnet-kube-b" {
   v4_cidr_blocks = ["192.168.20.0/24"]
   zone           = "ru-central1-b"
   network_id     = yandex_vpc_network.net-master.id
+  route_table_id = yandex_vpc_route_table.net-router.id
 }
 
 # Подсеть Kubernetes кластера в зоне доступности В
@@ -44,4 +47,5 @@ resource "yandex_vpc_subnet" "subnet-kube-c" {
   v4_cidr_blocks = ["192.168.30.0/24"]
   zone           = "ru-central1-c"
   network_id     = yandex_vpc_network.net-master.id
+  route_table_id = yandex_vpc_route_table.net-router.id
 }
